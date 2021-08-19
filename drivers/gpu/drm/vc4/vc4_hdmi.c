57eea994374888b00b64d8a8e02818547fe2af30
Maxime Ripard <maxime@cerno.tech>
2021-08-19 15:59:30 +0200

    drm/vc4: hdmi: Make sure the device is powered with CEC
    Commit 20b0dfa86bef0e80b41b0e5ac38b92f23b6f27f9 upstream.
    
    The original commit depended on a rework commit (724fc856c09e ("drm/vc4:
    hdmi: Split the CEC disable / enable functions in two")) that
    (rightfully) didn't reach stable.
    
    However, probably because the context changed, when the patch was
    applied to stable the pm_runtime_put called got moved to the end of the
    vc4_hdmi_cec_adap_enable function (that would have become
    vc4_hdmi_cec_disable with the rework) to vc4_hdmi_cec_init.
    
    This means that at probe time, we now drop our reference to the clocks
    and power domains and thus end up with a CPU hang when the CPU tries to
    access registers.
    
    The call to pm_runtime_resume_and_get() is also problematic since the
    .adap_enable CEC hook is called both to enable and to disable the
    controller. That means that we'll now call pm_runtime_resume_and_get()
    at disable time as well, messing with the reference counting.
    
    The behaviour we should have though would be to have
    pm_runtime_resume_and_get() called when the CEC controller is enabled,
    and pm_runtime_put when it's disabled.
    
    We need to move things around a bit to behave that way, but it aligns
    stable with upstream.
    
    Cc: <stable@vger.kernel.org> # 5.10.x
    Cc: <stable@vger.kernel.org> # 5.15.x
    Cc: <stable@vger.kernel.org> # 5.16.x
    Reported-by: Michael Stapelberg <michael+drm@stapelberg.ch>
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/vc4/vc4_hdmi.c
+++ drivers/gpu/drm/vc4/vc4_hdmi.c
@@ -2351,41 +2351,48 @@ static int vc4_hdmi_cec_enable(struct cec_adapter *adap)
 	u32 val;
 	int ret;
 
-	ret = pm_runtime_resume_and_get(&vc4_hdmi->pdev->dev);
-	if (ret)
-		return ret;
+	if (enable) {
+		ret = pm_runtime_resume_and_get(&vc4_hdmi->pdev->dev);
+		if (ret)
+			return ret;
 
-	val = HDMI_READ(HDMI_CEC_CNTRL_5);
-	val &= ~(VC4_HDMI_CEC_TX_SW_RESET | VC4_HDMI_CEC_RX_SW_RESET |
-		 VC4_HDMI_CEC_CNT_TO_4700_US_MASK |
-		 VC4_HDMI_CEC_CNT_TO_4500_US_MASK);
-	val |= ((4700 / usecs) << VC4_HDMI_CEC_CNT_TO_4700_US_SHIFT) |
-	       ((4500 / usecs) << VC4_HDMI_CEC_CNT_TO_4500_US_SHIFT);
+		val = HDMI_READ(HDMI_CEC_CNTRL_5);
+		val &= ~(VC4_HDMI_CEC_TX_SW_RESET | VC4_HDMI_CEC_RX_SW_RESET |
+			 VC4_HDMI_CEC_CNT_TO_4700_US_MASK |
+			 VC4_HDMI_CEC_CNT_TO_4500_US_MASK);
+		val |= ((4700 / usecs) << VC4_HDMI_CEC_CNT_TO_4700_US_SHIFT) |
+			((4500 / usecs) << VC4_HDMI_CEC_CNT_TO_4500_US_SHIFT);
 
-	HDMI_WRITE(HDMI_CEC_CNTRL_5, val |
-		   VC4_HDMI_CEC_TX_SW_RESET | VC4_HDMI_CEC_RX_SW_RESET);
-	HDMI_WRITE(HDMI_CEC_CNTRL_5, val);
-	HDMI_WRITE(HDMI_CEC_CNTRL_2,
-		   ((1500 / usecs) << VC4_HDMI_CEC_CNT_TO_1500_US_SHIFT) |
-		   ((1300 / usecs) << VC4_HDMI_CEC_CNT_TO_1300_US_SHIFT) |
-		   ((800 / usecs) << VC4_HDMI_CEC_CNT_TO_800_US_SHIFT) |
-		   ((600 / usecs) << VC4_HDMI_CEC_CNT_TO_600_US_SHIFT) |
-		   ((400 / usecs) << VC4_HDMI_CEC_CNT_TO_400_US_SHIFT));
-	HDMI_WRITE(HDMI_CEC_CNTRL_3,
-		   ((2750 / usecs) << VC4_HDMI_CEC_CNT_TO_2750_US_SHIFT) |
-		   ((2400 / usecs) << VC4_HDMI_CEC_CNT_TO_2400_US_SHIFT) |
-		   ((2050 / usecs) << VC4_HDMI_CEC_CNT_TO_2050_US_SHIFT) |
-		   ((1700 / usecs) << VC4_HDMI_CEC_CNT_TO_1700_US_SHIFT));
-	HDMI_WRITE(HDMI_CEC_CNTRL_4,
-		   ((4300 / usecs) << VC4_HDMI_CEC_CNT_TO_4300_US_SHIFT) |
-		   ((3900 / usecs) << VC4_HDMI_CEC_CNT_TO_3900_US_SHIFT) |
-		   ((3600 / usecs) << VC4_HDMI_CEC_CNT_TO_3600_US_SHIFT) |
-		   ((3500 / usecs) << VC4_HDMI_CEC_CNT_TO_3500_US_SHIFT));
+		HDMI_WRITE(HDMI_CEC_CNTRL_5, val |
+			   VC4_HDMI_CEC_TX_SW_RESET | VC4_HDMI_CEC_RX_SW_RESET);
+		HDMI_WRITE(HDMI_CEC_CNTRL_5, val);
+		HDMI_WRITE(HDMI_CEC_CNTRL_2,
+			   ((1500 / usecs) << VC4_HDMI_CEC_CNT_TO_1500_US_SHIFT) |
+			   ((1300 / usecs) << VC4_HDMI_CEC_CNT_TO_1300_US_SHIFT) |
+			   ((800 / usecs) << VC4_HDMI_CEC_CNT_TO_800_US_SHIFT) |
+			   ((600 / usecs) << VC4_HDMI_CEC_CNT_TO_600_US_SHIFT) |
+			   ((400 / usecs) << VC4_HDMI_CEC_CNT_TO_400_US_SHIFT));
+		HDMI_WRITE(HDMI_CEC_CNTRL_3,
+			   ((2750 / usecs) << VC4_HDMI_CEC_CNT_TO_2750_US_SHIFT) |
+			   ((2400 / usecs) << VC4_HDMI_CEC_CNT_TO_2400_US_SHIFT) |
+			   ((2050 / usecs) << VC4_HDMI_CEC_CNT_TO_2050_US_SHIFT) |
+			   ((1700 / usecs) << VC4_HDMI_CEC_CNT_TO_1700_US_SHIFT));
+		HDMI_WRITE(HDMI_CEC_CNTRL_4,
+			   ((4300 / usecs) << VC4_HDMI_CEC_CNT_TO_4300_US_SHIFT) |
+			   ((3900 / usecs) << VC4_HDMI_CEC_CNT_TO_3900_US_SHIFT) |
+			   ((3600 / usecs) << VC4_HDMI_CEC_CNT_TO_3600_US_SHIFT) |
+			   ((3500 / usecs) << VC4_HDMI_CEC_CNT_TO_3500_US_SHIFT));
 
-	if (!vc4_hdmi->variant->external_irq_controller)
-		HDMI_WRITE(HDMI_CEC_CPU_MASK_CLEAR, VC4_HDMI_CPU_CEC);
+		if (!vc4_hdmi->variant->external_irq_controller)
+			HDMI_WRITE(HDMI_CEC_CPU_MASK_CLEAR, VC4_HDMI_CPU_CEC);
+	} else {
+		if (!vc4_hdmi->variant->external_irq_controller)
+			HDMI_WRITE(HDMI_CEC_CPU_MASK_SET, VC4_HDMI_CPU_CEC);
+		HDMI_WRITE(HDMI_CEC_CNTRL_5, val |
+			   VC4_HDMI_CEC_TX_SW_RESET | VC4_HDMI_CEC_RX_SW_RESET);
 
-	spin_unlock_irqrestore(&vc4_hdmi->hw_lock, flags);
+		pm_runtime_put(&vc4_hdmi->pdev->dev);
+	}
 
 	return 0;
 }
@@ -2562,8 +2569,6 @@ static int vc4_hdmi_cec_init(struct vc4_hdmi *vc4_hdmi)
 	ret = cec_register_adapter(vc4_hdmi->cec_adap, &pdev->dev);
 	if (ret < 0)
 		goto err_remove_handlers;
-
-	pm_runtime_put(&vc4_hdmi->pdev->dev);
 
 	return 0;
 
