42df63877ffdaa32fc78f15f4d0af7d94e545b0d
parkjongheum <whdgmawkd@naver.com>
2021-12-08 18:27:07 +0900

    script: build_kernel: change dtb install path

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,4 +2,4 @@
 make bcm2711_rpi4_defconfig
 make bindeb-pkg KDEB_PKGVERSION=$(make kernelversion) KERNELRELEASE=5.15 LOCALVERSION= -j8
 
-INSTALL_PATH=./out make dtbs_install
+INSTALL_PATH=../out make dtbs_install
