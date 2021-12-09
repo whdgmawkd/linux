f9f4720bea90c3e985b148c7b79240f65f21aa9a
parkjongheum <whdgmawkd@naver.com>
2021-12-09 13:37:20 +0900

    script: build_kernel: change version string

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 make bcm2711_rpi4_defconfig
-make bindeb-pkg KDEB_PKGVERSION=$(make kernelversion) KERNELRELEASE=5.15 LOCALVERSION= -j8
+make bindeb-pkg KDEB_PKGVERSION=$(make kernelversion)-$(git rev-parse --short HEAD) KERNELRELEASE=5.15.6 LOCALVERSION= -j8
 
 INSTALL_PATH=../out make dtbs_install
