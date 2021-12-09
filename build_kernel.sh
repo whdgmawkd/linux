0a42c2f4aee815f00fda77e3210cabfa4ca50b02
parkjongheum <whdgmawkd@naver.com>
2021-12-09 16:25:21 +0900

    script: build_kernel: update package name rule

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+# rm ../*.{deb,changes,buildinfo}
+
 make bcm2711_rpi4_defconfig
-make bindeb-pkg KDEB_PKGVERSION=$(make kernelversion)-$(git rev-parse --short HEAD) KERNELRELEASE=5.15.6 LOCALVERSION= -j8
+make bindeb-pkg KDEB_PKGVERSION=$(make kernelversion)-$(date "+%s") KERNELRELEASE=5.15.6-arm64 LOCALVERSION= -j8
 
 INSTALL_PATH=../out make dtbs_install
